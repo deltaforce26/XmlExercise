af6645355cc6b44010bf680925e696a848f8e2c5
deltaforce26 <ahron052715@gmail.com>
2024-07-21 12:40:28 +0300

    Add project files.

---
+++ XmlExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using FileHandler.Services;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Serialization;
+namespace XmlExercise
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            
+            XMLService xmlService = new XMLService("quiz.xml");
+            int userChoice = 1;
+            
+            while (userChoice != 0)
+            {
+                Q_aModel model = new Q_aModel();
+                Console.WriteLine("1. Create questions");
+                Console.WriteLine("2.Answer Questions");
+                userChoice = Convert.ToInt32(Console.ReadLine());
+                if (userChoice == 1)
+                {
+                    Console.WriteLine("Type your question: ");
+                    model.question = Console.ReadLine();
+                    Console.WriteLine("Type your answer: ");
+                    model.answer = Console.ReadLine();
+                }
+                else if (userChoice == 2)
+                {
+                   XmlDocument doc = xmlService.ReadXML()!;
+                    foreach (XmlNode node in doc)
+                    {
+                        
+                        Console.WriteLine(node);
+                    }
+                }
+                xmlService.CreateNodeFromModel<Q_aModel>(model);
+                
+            }
+            
+
+
+        }
+    }
+
+    
+    
+}
---
+++ XmlExercise/Q&aModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlExercise
+{
+    public class Q_aModel
+    {
+        public string? question {  get; set; }
+        public string? answer { get; set; }
+    }
+}
